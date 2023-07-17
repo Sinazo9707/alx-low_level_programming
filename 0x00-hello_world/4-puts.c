c9d10d65ad5658e54542812f0419873e0dd1f682
Sinazo Ngceke <nazongceke@gmail.com>
2023-07-17 15:33:06 -0700

    5-printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (sucess)
  */
 int main(void)
-[
-	puts("\"Programming is like building a multilingual puzzle\n""); 
+{
+	puts("\"Programming is like building a multilingual puzzle\n"); 
 	return (0);
-]	
+}	
